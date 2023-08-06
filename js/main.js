5a617d355e209e9cfbb2c3e450d5cb549b6f82fa
thusitha <nuwan12403482@gmail.com>
2023-08-07 02:15:12 +0530

    Implemented checkGameOver() & display wining text

--- js/main.js
+++ js/main.js
@@ -14,13 +14,52 @@ const playerBulletController = new BulletController(canvas, 10, "red", true);
 const invaderBulletController = new BulletController(canvas, 4, "white", false);
 const invadersController = new InvaderController(canvas,invaderBulletController,playerBulletController);
 const player = new Player(canvas,5, playerBulletController);
-function shield(){
-    context.drawImage(background,0,0,canvas.width,canvas.height);
-    invadersController.draw(context);
-    player.draw(context);
-    playerBulletController.draw(context);
-    invaderBulletController.draw(context);
 
+let isGameOver = false;
+let didWin = false;
+
+
+
+function shield(){
+    checkGameOver();
+    context.drawImage(background,0,0,canvas.width,canvas.height);
+    displayGameOver();
+    if (!isGameOver) {
+        invadersController.draw(context);
+        player.draw(context);
+        playerBulletController.draw(context);
+        invaderBulletController.draw(context);
+    }
+
+}
+function checkGameOver() {
+    if (isGameOver) {
+        return;
+    }
+
+    if (invaderBulletController.collideWith(player)) {
+        isGameOver = true;
+    }
+
+    if (invadersController.collideWith(player)) {
+        isGameOver = true;
+    }
+
+    if (invadersController.invaderRows.length === 0) {
+        didWin = true;
+        isGameOver = true;
+    }
+}
+
+function displayGameOver() {
+    if (isGameOver) {
+        let text = didWin ? "You Win" : "Game Over";
+        let textOffset = didWin ? 3.1 : 3.1;
+
+        context.fillStyle = "white";
+        context.font = "70px Arial";
+        context.fillText(text, canvas.width / textOffset , canvas.height / 2);
+    }
 }
 
 setInterval(shield,1000/60);

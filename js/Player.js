cf7c865117498e8588f9e23dba50148a6a4bbdd3
thusitha <nuwan12403482@gmail.com>
2023-08-06 23:35:51 +0530

    Implemented moving function with collision wall detection

--- js/Player.js
+++ js/Player.js
@@ -1,18 +1,62 @@
-export default class player{
-    constructor(canvas, velocity){
+export default class player {
+    rightPressed = false;
+    leftPressed = false;
+
+    constructor(canvas, velocity) {
         this.canvas = canvas;
         this.velocity = velocity;
 
-        this.x = this.canvas.width / 2 -75;
+        this.x = this.canvas.width / 2 - 75;
         this.y = this.canvas.height - 100;
         this.width = 150;
         this.height = 150;
         this.image = new Image();
         this.image.src = "img/player.gif";
 
+        document.addEventListener("keydown", this.keydown);
+        document.addEventListener("keyup", this.keyup);
     }
 
-    draw(context){
+    draw(context) {
         context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        this.move();
+        this.collideWithWalls();
     }
+
+    keydown = (event) => {
+        if (event.code == "ArrowRight") {
+            this.rightPressed = true;
+        }
+        if (event.code == "ArrowLeft") {
+            this.leftPressed = true;
+        }
+    }
+    keyup = (event) => {
+        if (event.code == "ArrowRight") {
+            this.rightPressed = false;
+        }
+        if (event.code == "ArrowLeft") {
+            this.leftPressed = false;
+        }
+    }
+    move() {
+        if (this.rightPressed) {
+            this.x += this.velocity;
+        } else if (this.leftPressed) {
+            this.x += -this.velocity;
+        }
+    }
+
+    collideWithWalls() {
+        //left
+        if (this.x < -75) {
+            this.x = -75;
+        }
+
+        //right
+        if (this.x > this.canvas.width - this.width/2) {
+            this.x = this.canvas.width - this.width/2;
+        }
+    }
+
 }

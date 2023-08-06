23d14496a95dd20fe71cb070227d77306aef595d
thusitha <nuwan12403482@gmail.com>
2023-08-06 16:24:40 +0530

    Create new InvaderController.js & called draw() method

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log($ == jQuery);
+import InvaderController from "./InvaderController.js";
 const canvas = $('#game')[0];
 const context = canvas.getContext("2d");
 
@@ -8,8 +8,11 @@ canvas.height = 500;
 const background = new Image();
 background.src = "img/Star Wars Space Background 69 images.jpg";
 
+const invadersController = new InvaderController(canvas);
+
 function shield(){
     context.drawImage(background,0,0,canvas.width,canvas.height);
+    invadersController.draw(context);
 }
 
 setInterval(shield,1000/60);

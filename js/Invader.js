279b6ded133f31f62bdbae244290bb1cb8471324
thusitha <nuwan12403482@gmail.com>
2023-08-07 02:15:51 +0530

    Implemented collideWith() function

--- js/Invader.js
+++ js/Invader.js
@@ -16,4 +16,16 @@ export default class Invader {
         this.x += xVelocity;
         this.y += yVelocity;
     }
+    collideWith(player) {
+        if (
+            this.x + this.width > player.x &&
+            this.x < player.x + player.width &&
+            this.y + this.height > player.y &&
+            this.y < player.y + player.height
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
